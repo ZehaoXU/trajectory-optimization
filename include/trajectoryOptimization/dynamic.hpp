081ca1e8c0a3a691c2393adff56673417a03bb8e
jasper <244155994@qq.com>
2018-08-04 06:38:32 +0800

    add class mujocoDynamic to dynamic.hpp

--- include/trajectoryOptimization/dynamic.hpp
+++ include/trajectoryOptimization/dynamic.hpp
@@ -43,4 +43,44 @@ namespace trajectoryOptimization::dynamic {
 		return {nextPosition, nextVelocity};
 	}
 
+	class GetNextPositionVelocityUsingMujoco
+	{
+	private:
+		mjModel* m;
+		mjData* d;
+		const int worldDimension;
+		const double dt;
+	public:
+		GetNextPositionVelocityUsingMujoco(const mjModel* model, mjData* data, const int dimension, const double dTime):
+			worldDimension(dimension), dt(dTime)
+			{
+				m = mj_copyModel(NULL, model);
+				d = mj_copyData(NULL, m, data);
+			}
+		std::tuple<dvector, dvector> operator()(const double* position,
+												const double* velocity,
+												const double* control)
+		{
+			mju_copy(d->qpos, position, worldDimension); 
+			mju_copy(d->qvel, velocity, worldDimension);
+
+			mjtNum startTime = d->time;
+			while(d->time - startTime < dt)
+			{
+				mj_step1(m, d);
+				mju_copy(d->ctrl, control, 3);
+				mj_step2(m, d);
+			}
+			
+			dvector nextPosition(d->qpos, d->qpos + 3);
+			dvector nextVelocity(d->qvel, d->qvel + 3);
+
+			mj_resetData(m, d);
+
+			return {nextPosition, nextVelocity};
+		}
+	};
+
 }
+
+
--- test/src/mujocoDynamicTest.cpp
+++ test/src/mujocoDynamicTest.cpp
@@ -4,12 +4,12 @@
 #include <string>
 #include <stdlib.h>
 #include "mujoco.h"
-#include "trajectoryOptimization/mujocoDynamic.hpp"
+#include "trajectoryOptimization/dynamic.hpp"
 
 
 
 // using namespace testing;
-using namespace trajectoryOptimization::mujocoDynamic;
+using namespace trajectoryOptimization::dynamic;
 using namespace std;    // take care
 
 mjModel* m = NULL;
@@ -39,26 +39,23 @@ int main(int argc, const char ** argv)
 
     d = mj_makeData(m);
 
+    int worldDimension = 3;
+
+    double dTime = 0.5;
+
     // qpos=nq=7, qvel=nv=6, ctrl=nu=0, qfrc_applied=nv=6
     
-    // test1: change pos vel, return acc
     dvector position = {1,2,0.2};
     dvector velocity = {2,2,0};
-    dvector control = {1,2,98.1};
-    const double* acc = getAccUsingMujoco(m, d, position.data(), velocity.data(), control.data());
-    cout << "Test1: change pos vel ctrl and return acc" << endl;
-    cout << "pos: " << d->qpos[0] << "  " << d->qpos[1] << "  " << d->qpos[2] << endl;
-    cout << "vel: " << d->qvel[0] << "  " << d->qvel[1] << "  " << d->qvel[2] << endl;
-    cout << "acc: " << d->qacc[0] << "  " << d->qacc[1] << "  " << d->qacc[2] << endl;
-    cout << "ctrl:" << d->qfrc_applied[0] << "  "
-                    << d->qfrc_applied[1] << "  "
-                    << d->qfrc_applied[2] << "  " << endl << endl;
+    dvector control = {1,2,0};
 
     // test2: step forward, return tuple<vector(position), vector(velocity)>
     cout << "Test2: step forward, return next position and next velocity" << endl;
     
-    double dTime = 0.5;
-    auto nextTimePoint = stepForward(m, d, position.data(), velocity.data(), control.data(),dTime);
+    auto getNexPosVel = GetNextPositionVelocityUsingMujoco(m, d, worldDimension, dTime);
+
+    auto nextTimePoint = getNexPosVel(position.data(), velocity.data(), control.data());
+    
     cout << "next pos: " << get<0>(nextTimePoint)[0] << "  " 
                         << get<0>(nextTimePoint)[1] << "  "
                         << get<0>(nextTimePoint)[2] << endl;
